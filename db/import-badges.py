e3266335f417b92ab28a6c1352563f718a4a20dc
Nik0l <nburlutsky@hotmail.com>
2015-08-07 10:44:29 +0100

    Update import-badges.py

--- db/import-badges.py
+++ db/import-badges.py
@@ -1,2 +1,2 @@
 # importing xml files of Stack Exchange into sqlite3 database,
-the source is taken from: http://www.cs.berkeley.edu/~bjoern/projects/stackoverflow/
+# a part of the code is taken from: http://www.cs.berkeley.edu/~bjoern/projects/stackoverflow/
