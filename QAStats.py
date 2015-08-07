df9122528172c5142160f9804b695d65bd211892
Nik0l <nburlutsky@hotmail.com>
2015-08-07 10:28:55 +0100

    Create QAStats.py

---
+++ QAStats.py
@@ -0,0 +1,1 @@
+# Calculating statistics on Q&A communities. Includes temporal statistics ( for example, how many questions were asked in a period), spatial statistics, number of users, questions, answers.
