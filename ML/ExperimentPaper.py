35a2ef4532b104a74335bf050d1695767860b5d8
Nikolay Burlutskiy <nburlutsky@hotmail.com>
2016-03-02 19:38:21 +0000

    Update ExperimentPaper.py

--- ML/ExperimentPaper.py
+++ ML/ExperimentPaper.py
@@ -1,111 +1,51 @@
-__author__ = 'nb254'
-
-import numpy as np
-import pandas as pd
-import MachineLearning as ml
-import time
+import MLSetup as ml
 import OfflineLearning as offlineML
 import OnlineLearning as onlineML
-
-feature_predict = ['TEMP_CL']
-
-def runExperiment(df, feature_sets, classifiers, times, batch_size=5000):
-    tick = time.time()
-    outputs = []
-    for feature_set in feature_sets:
-        y, X = ml.getDataForML(df, features=feature_set, feature_predict=feature_predict, sampling=False)
-        n_features = X.shape[1]
-        print 'total features ', n_features
-        print 'total samples ', X.shape[0]
-        times['preparing_time'] += time.time() - tick
-
-        cls_stats = offlineML.runOfflineML(y, X, classifiers['online'])
-        output = [cls_stats, classifiers['online'], feature_set, '5fold']
-        outputs.append(output)
-        # online
-        cls_stats = onlineML.runOnlineML(y, X, classifiers['online'], batch_size=batch_size)
-        output = [cls_stats, classifiers['online'], feature_set, '5000batch']
-        outputs.append(output)
-
-        cls_stats = offlineML.runOfflineML(y, X, classifiers['offline'])
-        output = [cls_stats, classifiers['offline'], feature_set, '5fold']
-        outputs.append(output)
-
-        cls_stats = onlineML.runOnlineML(y, X, classifiers['offline'], batch_size=batch_size)
-        output = [cls_stats, classifiers['offline'], feature_set, '5000batch']
-        outputs.append(output)
-        #saveClassificationResults(DIR + 'results/accuracy_ML.csv', output)
-        #theanoTest(y,X)
-        #pt.plotEverything(cls_stats, times, len(X))
-    return outputs
-
-def runExpNtimes(df, feature_sets, classifiers, repetitions):
-    times = {'preparing_time':0.0,
-             'training_time': 0.0,
-             'testing_time': 0.0,
-             'another_time': 0.0}
-    results = []
-    for index in xrange(0, repetitions):
-        outputs = runExperiment(df, feature_sets, classifiers, times)
-        results.append(makeResults(outputs))
-    table = []
-    for result in results:
-        for res in result:
-            table.append(res)
-    dfres = pd.DataFrame(table, columns=['Algorithm', 'Learning_Type', 'Features', 'Accuracy', 'Training_Time', 'Testing_Time'])
-    return dfres
-
-def makeResults(outputs):
-    results = []
-    for output in outputs:
-        cls_stats = output[0]
-        classifiers = output[1]
-        feature_set = output[2]
-        learning_type = output[3]
-        for cls_name, cls in classifiers.items():
-            results.append([cls_name, learning_type, feature_set, cls_stats[cls_name]['accuracy'],
-                            cls_stats[cls_name]['training_time'], cls_stats[cls_name]['testing_time']])
-    return results
-
-def makeFirstTable(dfres):
-    dfresa = dfres[dfres['Features']=='all']
-    dfresa = dfresa.groupby(['Learning_Type', 'Algorithm', 'Features'])
-    #print dfres
-    dfres1 = dfresa['Accuracy'].agg({'Accuracy_m': np.mean, 'Accuracy_std': np.std})
-    dfres2 = dfresa['Training_Time'].agg({'Training_Time_m': np.mean})
-    dfres3 = dfresa['Testing_Time'].agg({'Testing_Time_m': np.mean})
-    dfresa = pd.concat([dfres1, dfres2, dfres3], axis=1)
-    dfresa = dfresa.reset_index()
-    dfresa['Accuracy_m'] = 100 * np.round(dfresa['Accuracy_m'], 3)
-    dfresa['Accuracy_std'] = np.round(dfresa['Accuracy_std'], 2)
-    dfresa['Training_Time_m'] = np.round(dfresa['Training_Time_m'], 1)
-    dfresa['Testing_Time_m'] = np.round(dfresa['Testing_Time_m'], 3)
-    return dfresa
-
-def makeSecondTable(dfres):
-    dfresb = dfres[dfres['Learning_Type']=='5fold']
-    dfresb = dfresb.groupby(['Algorithm', 'Features'])
-    dfresb = dfresb['Accuracy'].agg({'Accuracy_m': np.mean})
-    dfresb = dfresb.reset_index()
-    dfresb['Accuracy_m'] = 100 * np.round(dfresb['Accuracy_m'], 3)
-    return dfresb
-
+import pandas as pd
+import MergeFeatures as mf
+import os
+import FeatureExtractor as extractor
 # the path to the file with data
-DIR = '/mnt/nb254_data/src/data_SO/data_for_ML/'
+
+def extractFeaturesML(db_name, DIR):
+    key_users = ['UserId']
+    key_posts = ['PostId']
+    # extracts all features
+    extractor.extractForML(db_name, DIR)
+    dfML = mf.mergeAll(DIR, key_users, key_posts, mf.FeaturePredict)
+    dfML.to_csv(DIR + 'dataML.csv', index=False)
+    dfnorm = mf.normalize(dfML)
+    dfnorm = dfnorm.fillna(0)
+    return dfnorm
+
+def run_ML(df):
+    feature_sets = ['RL', 'NOTAGS', 'NOSTEMP', 'all']
+
+    classifiers = dict(
+        online=onlineML.getOnlineClassifiers(),
+        offline=offlineML.getOfflineClassifiers(df.shape[1])
+    )
+
+    dfres = ml.runExpNtimes(df, feature_sets, classifiers, repetitions=2)
+    dfres = dfres.sort(['Learning_Type', 'Algorithm', 'Features'], ascending=False)
+    dfresa = ml.makeFirstTable(dfres)
+    os.makedirs(DIR + 'results/')
+    dfresa.to_csv(DIR + 'results/results_algorithms.csv')
+    dfresb = ml.makeSecondTable(dfres)
+    dfresb.to_csv(DIR + 'results/results_features.csv', index=False)
+    print dfresb
+
+#DIR = '/mnt/nb254_data/src/data_SO/data_for_ML/'
+#db_name = 'beer'
+db_name = 'stackoverflow'
+DIR = '/mnt/nb254_data/exp/'
+subdir = 'exp_' + db_name
+if not os.path.exists(DIR + subdir):
+        os.makedirs(DIR + subdir)
+DIR = DIR + subdir + '/'
+
+df = extractFeaturesML(db_name, DIR)
+df.to_csv(DIR + 'normMLa.csv', index=False)
 filename = 'normMLa.csv'
-
 df = pd.read_csv(DIR + filename)
-
-feature_sets = ['RL', 'NOTAGS', 'NOSTEMP', 'all']
-classifiers = dict(
-    online=onlineML.getOnlineClassifiers(),
-    offline=offlineML.getOfflineClassifiers(df.shape[1])
-)
-
-dfres = runExpNtimes(df, feature_sets, classifiers, repetitions=10)
-dfres = dfres.sort(['Learning_Type', 'Algorithm', 'Features'], ascending=False)
-dfresa = makeFirstTable(dfres)
-dfresa.to_csv('results1.csv')
-dfresb = makeSecondTable(dfres)
-dfresb.to_csv('results2.csv', index=False)
-print dfresb
+run_ML(df)
