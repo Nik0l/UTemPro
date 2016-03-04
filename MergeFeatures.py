e603128e2d75bb0d0744f70da87687cebb7833ab
Nikolay Burlutskiy <nburlutsky@hotmail.com>
2016-03-04 10:09:10 +0000

    Update MergeFeatures.py

--- MergeFeatures.py
+++ MergeFeatures.py
@@ -1,19 +1,7 @@
 __author__ = 'nb254'
 
 import pandas as pd
-
-UserFeatures = ['U_QUESTIONS', 'U_ANSWERS', 'NUM_OF_A_ACCEPTED_BY_OTHERS',
-                'U_AV_ANS_TIME', 'DAYS_ON_SITE', 'U_REPUTATION', 'U_UPVOTES', 'U_DOWNVOTES',
-                'LAT', 'LON', 'LOCATION_NUM', 'time_diff', 'U_VIEWS', 'qh1', 'qh2',
-                'qh3', 'qh4', 'qh5', 'qh6', 'qh7', 'qh8', 'qh9', 'qh10', 'qh11', 'qh12', 'qh13', 'qh14',
-                'qh15', 'qh16', 'qh17', 'qh18', 'qh19', 'qh20', 'qh21', 'qh22', 'qh23', 'qh24',
-                'qm1', 'qm2', 'qm3', 'qm4', 'qm5', 'qm6', 'qm7', 'qm8', 'qm9', 'qm10', 'qm11',
-                'qm12', 'Q_LAST_WEEK', 'A_LAST_WEEK', 'P_NUM_LAST_DAY']
-
-PostsFeatures = ['UserId', 'Q_VIEWS', 'WH_TITLE', 'WH_BODY', 'VERBS_TITLE', 'VERBS_BODY', 'SELF_REF_BODY',
-                 'TITLE_LENGTH', 'BODY_LENGTH', 'TAG_POPULARITY_AV', 'NUM_POP_TAGS', 'TAG_SPECIFICITY',
-                 'NUM_TAGS', 'URLS_BODY', 'IMAGES_BODY']
-FeaturePredict = ['TEMP_CL']
+import FeatureSelector as fs
 
 def mergePostFeatures(DIR):
     df = pd.read_csv(DIR + 'posts/quest_stats.csv')
@@ -54,28 +42,32 @@ def mergeUserFeatures(DIR):
 def cleanData(df):
     #TODO clean the data
     df = df.fillna(0)
-    col_names = UserFeatures + PostsFeatures
+    col_names = fs.UserFeatures + fs.PostsFeatures
     col_names.remove('TAG_SPECIFICITY')
+
+    rows_with_strings  = df.apply(
+       lambda row :
+          any([ isinstance(e, basestring) for e in row ])
+       , axis=1)
+    df = df[~rows_with_strings]
+    #print ~df.applymap(pd.np.isreal)
+    print df['LON'].median()
+    print df['LAT'].median()
     for col_name in col_names:
-       df[col_name] = df[col_name].astype(int)
+        print col_name
+        df[col_name] = df[col_name].astype(int)
     return df
 
 def mergeAll(DIR, key_users, key_posts, FeaturePredict):
     df_p = mergePostFeatures(DIR)
-    df_p = df_p[key_posts + PostsFeatures + FeaturePredict]
+    df_p = df_p[key_posts + fs.PostsFeatures + FeaturePredict]
     #print 'samples in posts', len(df_p)
     df_p.to_csv(DIR + 'posts_all.csv')
     df_u = mergeUserFeatures(DIR)
-    df_u = df_u[key_users + UserFeatures]
+    df_u = df_u[key_users + fs.UserFeatures]
     #print 'samples in users', len(df_p)
     df_u.to_csv(DIR + 'users_all.csv')
-    df_ML = df_res = pd.merge(df_p, df_u, how='left', on='UserId')
+    df_ML = pd.merge(df_p, df_u, how='left', on='UserId')
     df_ML = cleanData(df_ML)
     return df_ML
-
-def normalize(df):
-    df_norm = (df - df.mean()) / (df.max() - df.min())
-    df_norm = df_norm - df_norm.min()
-    dfnorm = dfnorm.fillna(0)
-    return df_norm
 
